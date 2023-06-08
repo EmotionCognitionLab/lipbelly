27687d85cfc8381c7670fceaeffc713dc2261dd2
Noah Mercer <noah.mercer@gmail.com>
2023-06-08 13:21:57 +0300

    Adds styling

--- client/src/renderer/App.vue
+++ client/src/renderer/App.vue
@@ -11,5 +11,26 @@
 </script>
 
 <style>
-  /* CSS */
+ body {
+  font-family: Arial, sans-serif;
+  font-size: 22px;
+  line-height: 1.6em;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: beige;
+  height: 100%;
+  width: 100%;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+}
+.instruction {
+    max-width: 40em;
+    padding: 2.5%;
+    margin: auto;
+}
 </style>
