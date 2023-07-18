d03f27ab7f003e1ea26963966160cf74f1330680
Noah Mercer <noah.mercer@gmail.com>
2023-07-18 15:58:59 -0700

    Code to select the 84 emo pics a participant sees

---
+++ client/src/emomem-selection.js
@@ -0,0 +1,37 @@
+/**
+ * Randomly selects the emotional memory pictures that a participant will see.
+ * The pictures are organized into three groups (A, B, and C) with 42 pictures each.
+ * Each picture is either neutral, positive or negative.
+ * We first randomly pick (without replacement) two of the three groups. We then
+ * pick (without replacement) two neutral, two positive and two negative pictures
+ * from those two groups and shuffle them (so that the participant doesn't always see two
+ * neutral pictures followed by two positive pictures, for example). We record those six
+ * (including which group each picture was part of) and then repeat the process until
+ *  we've selected 84 images in total.
+ */
+
+import imgData from './emopics.json'
+import {initJsPsych} from 'jspsych'
+
+const jspsych = initJsPsych();
+const shuffle = jspsych.randomization.shuffle;
+const sampleWithoutReplacement = jspsych.randomization.sampleWithoutReplacement;
+
+export function selectEmotionalImages() {
+    const groups = ['A', 'B', 'C'];
+    const selectedGroups = sampleWithoutReplacement(groups, 2);
+    const neut = shuffle(imgData['Neutral'].filter(img => selectedGroups.includes(img.group)));
+    const neg = shuffle(imgData['Negative'].filter(img => selectedGroups.includes(img.group)));
+    const pos = shuffle(imgData['Positive'].filter(img => selectedGroups.includes(img.group)));
+    const selectedImgs = [];
+
+    while (selectedImgs.length < 84) {
+        const tmp = [];
+        tmp.push(neut.pop()); tmp.push(neut.pop());
+        tmp.push(neg.pop()); tmp.push(neg.pop());
+        tmp.push(pos.pop()); tmp.push(pos.pop());
+        selectedImgs.push(...shuffle(tmp));
+    }
+
+    return selectedImgs;
+}
---
+++ client/test/emomem-selection.test.js
@@ -0,0 +1,64 @@
+/**
+ * @jest-environment jsdom
+ */
+import { selectEmotionalImages } from '../src/emomem-selection.js'
+import imgData from '../src/emopics.json';
+
+describe("Selecting emotional images", () => {
+    it("should have 84 items", () => {
+        const imgs = selectEmotionalImages();
+        expect(imgs.length).toBe(84);
+    });
+
+    it("should have images from only two groups", () => {
+        const imgs = selectEmotionalImages();
+        const groups = new Set(imgs.map(i => i.group));
+        expect(groups.size).toBe(2);
+        expect(['A', 'B', 'C']).toContain(...Array.from(groups));
+    });
+
+    it("should have two posititve, two negative and two neutral images in each block of six images", () => {
+        const imgs = selectEmotionalImages();
+        const neut = imgData['Neutral'];
+        const pos = imgData['Positive'];
+        const neg = imgData['Negative'];
+        for (let i = 0; i<imgs.length - 6; i+=6) {
+            const tmp = imgs.slice(i, i+6);
+            const neutCount = tmp.filter(i => neut.includes(i)).length;
+            expect(neutCount).toBe(2);
+            const negCount = tmp.filter(i => neg.includes(i)).length;
+            expect(negCount).toBe(2);
+            const posCount = tmp.filter(i => pos.includes(i)).length;
+            expect(posCount).toBe(2);
+        }
+    });
+
+    it("should not have pos/neg/neut images in the same order in every group of six", () => {
+        const imgs = selectEmotionalImages();
+
+        const neut = imgData['Neutral'];
+        const pos = imgData['Positive'];
+        const neg = imgData['Negative'];
+
+        const imgValence = (img) => {
+            if (neut.includes(img)) {
+                return "Neutral"
+            } else if (pos.includes(img)) {
+                return "Positive"
+            } else if (neg.includes(img)) {
+                return "Negative"
+            } else {
+                throw new Error(`img ${JSON.stringify(img)} is not neutral, positive or negative`);
+            }
+        };
+
+        const valenceArrs = [];
+        const valences = imgs.map(imgValence);
+        for (let i=0; i<valences.length; i+=6) {
+            valenceArrs.push(valences.slice(i, i+6));
+        }
+
+        expect(valenceArrs.every((val, idx) => valenceArrs[0][idx] == val[idx])).not.toBeTruthy();        
+
+    });
+});
